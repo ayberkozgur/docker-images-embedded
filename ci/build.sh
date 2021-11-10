1e935e2f1e9c71b09566e5a565cf5e6dfe8be196
ayberkozgur <ayberk.oezguer@gmail.com>
2021-11-10 14:35:22 +0100

    Uavcan image

--- ci/build.sh
+++ ci/build.sh
@@ -8,4 +8,4 @@ docker build --no-cache --tag ayberkozgur/stm32-ci:1.0 images/stm32-ci/
 rm -rf images/stm32-ci/scripts/
 
 # uavcan-ci
-#docker build --no-cache --tag ayberkozgur/uavcan-ci:1.0 images/uavcan-ci/
+docker build --no-cache --tag ayberkozgur/uavcan-ci:1.0 images/uavcan-ci/
--- ci/push.sh
+++ ci/push.sh
@@ -4,4 +4,4 @@ set -e
 
 docker login -u $DOCKERHUB_USER -p $DOCKERHUB_TOKEN
 docker push ayberkozgur/stm32-ci:1.0
-#docker push ayberkozgur/uavcan-ci:1.0
+docker push ayberkozgur/uavcan-ci:1.0
